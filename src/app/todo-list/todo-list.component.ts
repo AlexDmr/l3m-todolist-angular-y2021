641b76d838aba56a922d1d54c9ab785b0c79e757
demeurea <alexandre.demeure@univ-grenoble-alpes.fr>
2021-03-12 14:46:54 +0100

    correction séance 3

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { TodoItem, TodoList, TodolistService } from '../todolist.service';
+
+interface TodoAll extends TodoList {
+  remaining: number;
+  isAllDone: boolean;
+}
+
+type FctFilter = (item: TodoItem) => boolean;
 
 @Component({
   selector: 'app-todo-list',
@@ -9,14 +17,24 @@ import { TodoItem, TodoList, TodolistService } from '../todolist.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TodoListComponent implements OnInit {
+  readonly obsTodoAll: Observable<TodoAll>;
+  readonly fAll:       FctFilter = () => true;
+  readonly fCompleted: FctFilter = (it) => it.isDone;
+  readonly fActive:    FctFilter = (it) => !it.isDone;
+  f: FctFilter = this.fAll;
 
-  constructor(private TDLS: TodolistService) { }
+  constructor(private TDLS: TodolistService) {
+    this.obsTodoAll = this.TDLS.observable.pipe(
+      map( tdl => ({
+        ...tdl,
+        remaining: tdl.items.reduce( (n, it) => it.isDone ? n : n + 1, 0),
+        isAllDone: !tdl.items.find( this.fActive )
+      }) )
+    );
 
-  ngOnInit(): void {
   }
 
-  get obsTodoList(): Observable<TodoList> {
-    return this.TDLS.observable;
+  ngOnInit(): void {
   }
 
   append(label: string): void {
@@ -27,8 +45,16 @@ export class TodoListComponent implements OnInit {
     this.TDLS.update(u, item);
   }
 
+  updateItems(items: readonly TodoItem[], u: Partial<TodoItem>): void {
+    this.TDLS.update(u, ...items);
+  }
+
   delete(item: TodoItem): void {
     this.TDLS.remove(item);
+  }
+
+  deleteItems(items: readonly TodoItem[]) {
+    this.TDLS.remove( ...items );
   }
 
   trackById(i: number, e: TodoItem): number {
